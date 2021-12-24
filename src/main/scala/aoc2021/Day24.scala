eb8e5dc17b64849aba4ec0d9c8dde3890355ae0a
Misja Alma <malma@xebia.com>
2021-12-24 09:38:16 +0100

    day 24

--- src/main/scala/aoc2021/Day24.scala
+++ src/main/scala/aoc2021/Day24.scala
@@ -96,8 +96,8 @@ object Day24Part1 extends IOApp {
   }
 
   def findLargestValidNr(commands: Seq[Command]): Long = {
-    val input = Seq(0,0,0,0,0,0,0,0,0,0,0,0,0,0)
-    val vars = Map[String, Long]() + ("z" -> 7L)
+    val input = Seq(1,0,0,0,0,0,0,0,0,0,0,0,0,0)
+    val vars = Map[String, Long]() + ("z" -> 0L)
 
     val startState = State(vars, input)
     val finalState = evaluateInput(commands, startState)
@@ -105,36 +105,117 @@ object Day24Part1 extends IOApp {
     finalState.variables("z")
   }
 
+  case class InputFunction(par1: Int, par2: Int, divideZ: Int) {
+    def calc(w: Int, z: Long): Long = {
+      val x = z % 26 + par1
+      val z2 = z / divideZ // means that for outcome to be possibly 0, previous z had to be < +/- 26 for last steps
+      //    if (x == w) x = 0 else x = 1
+      //    z * (25*x + 1) + x*(w + par2)
+      //if (x == w) z else z * 26 + (w + par2)
+      if (x == w) z2 else z2 * 26 + w + par2
+    }
+  }
+
+  val functions = Seq[InputFunction] (
+    InputFunction(10,15,1),
+    InputFunction(12,8,1),
+    InputFunction(15,2,1),
+    InputFunction(-9,6,26),
+    InputFunction(15,13,1),
+    InputFunction(10,4,1),
+    InputFunction(14,1,1),
+    InputFunction(-5,9,26),
+    InputFunction(14,5,1),
+    InputFunction(-7,13,26),
+    InputFunction(-12,9,26),
+    InputFunction(-10,6,26),
+    InputFunction(-1,2,26),
+    InputFunction(-11,2,26)
+  )
+
+  def findHighestZero(functions: Seq[InputFunction], lastZ: Long): Option[Seq[Int]] = {
+    val next = functions.head
+    val rest = functions.tail
+    if (next.divideZ == 1) {
+      // just try all
+      (9 to 1 by -1).toList.to(LazyList).flatMap { w =>
+        val newZ = next.calc(w, lastZ)
+        if (rest.isEmpty) {
+          if (newZ == 0) Some(Seq(w)) else None
+        } else {
+          findHighestZero(rest, newZ).map { ws => w +: ws }
+        }
+      }.headOption
+    } else {
+      // take the highest possible based on lastZ
+      if (rest.isEmpty) {
+        if (lastZ >= 26) None else {
+          val newW = lastZ % 26 + next.par1
+          if (newW > 9 || newW < 1) None else Some(Seq(newW.toInt))
+        }
+      } else {
+        // fix W so that we get the division
+        val w = lastZ % 26 + next.par1
+        if (w > 9 || w < 1) None else {
+          val newZ = next.calc(w.toInt, lastZ)
+          findHighestZero(rest, newZ).map { ws => w.toInt +: ws }
+        }
+      }
+    }
+  }
+
+  def findLowestZero(functions: Seq[InputFunction], lastZ: Long): Option[Seq[Int]] = {
+    val next = functions.head
+    val rest = functions.tail
+    if (next.divideZ == 1) {
+      // just try all
+      (1 to 9).toList.to(LazyList).flatMap { w =>
+        val newZ = next.calc(w, lastZ)
+        if (rest.isEmpty) {
+          if (newZ == 0) Some(Seq(w)) else None
+        } else {
+          findLowestZero(rest, newZ).map { ws => w +: ws }
+        }
+      }.headOption
+    } else {
+      // take the highest possible based on lastZ
+      if (rest.isEmpty) {
+        if (lastZ >= 26) None else {
+          val newW = lastZ % 26 + next.par1
+          if (newW > 9 || newW < 1) None else Some(Seq(newW.toInt))
+        }
+      } else {
+        // fix W so that we get the division
+        val w = lastZ % 26 + next.par1
+        if (w > 9 || w < 1) None else {
+          val newZ = next.calc(w.toInt, lastZ)
+          findLowestZero(rest, newZ).map { ws => w.toInt +: ws }
+        }
+      }
+    }
+  }
+
+  def evaluateAll() = {
+    val input = Seq(1,0,0,0,0,0,0,0,0,0,0,0,0,0)
+    val z = 0L
+    val (_, result) = functions.foldLeft((input, z)) { case ((is, zs), f) =>
+      val newZ = f.calc(is.head, zs)
+      (is.tail, newZ)
+    }
+
+    println (result)
+  }
+
   override def run(args: List[String]): IO[ExitCode] = {
     for {
-      sc <- scannerFromResource("/test.txt")
+      sc <- scannerFromResource("/day24.txt")
       lines = scannerToLines(sc).filterNot(_.isBlank)
-      commands = lines.map(parseCommand)
-      solution = findLargestValidNr(commands)
-      _ <- IO.delay(println("Solution: " + solution))
+      _ <- IO.delay(println(findHighestZero(functions, 0).map(_.mkString(""))))
+      _ <- IO.delay(println(findLowestZero(functions, 0).map(_.mkString(""))))
+//      commands = lines.map(parseCommand)
+//      solution = findLargestValidNr(commands)
+//      _ <- IO.delay(println("Solution: " + solution))
     } yield ExitCode.Success
   }
 }
 
-object TestFunc extends  App {
-
-
-  def partFunc2(par1: Long, par2: Long)(w: Long, z: Long): Long = {
-    val x = z % 26 + par1
-    val z2 = z / 26
-    //    if (x == w) x = 0 else x = 1
-    //    z * (25*x + 1) + x*(w + par2)
-    if (x == w) z2 else w + par2
-  }
-
-  def partFunc(par1: Long, par2: Long, par3: Long)(w: Long, z: Long): Long = {
-    val x = z % 26 + par1
-    val z2 = z / par3   // means that for outcome to be possibly 0, previous z had to be < +/- 26 for last steps
-//    if (x == w) x = 0 else x = 1
-//    z * (25*x + 1) + x*(w + par2)
-    //if (x == w) z else z * 26 + (w + par2)
-    if (x == w) z2 else  z* 26 + w + par2    // modulo 26 is normally removed in next step but counts for par3 = 1
-  }
-
-  println (partFunc(-11, 2, 26)(9, 20))
-}
